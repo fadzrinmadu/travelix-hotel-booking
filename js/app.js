6ce017a84aa8eb3dfd3994e3181ae5f6e33a8970
fadzrinmadu <muhfadzrinmadu@gmail.com>
2020-08-30 21:24:44 +0700

    finished

--- js/app.js
+++ js/app.js
@@ -5,3 +5,8 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 hamburgerMenu.addEventListener('click', function() {
   header.classList.toggle('menu-open');
 });
+
+window.addEventListener('scroll', function() {
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle('active', windowPosition);
+});
